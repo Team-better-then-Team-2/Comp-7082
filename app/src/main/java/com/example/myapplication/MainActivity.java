8c9881a37a97cc248b4dd3fd9fe4f5f40f24c3bc
hughsong <hugh.happy.everyday@gmail.com>
2020-09-17 21:36:07 -0700

    Make Camera works, and store the photos. (Still did not set any info about photo, like name,caption....)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,12 +26,20 @@ import android.widget.ImageView;
 import com.example.myapplication.DataStorage.PhotoDao;
 import com.example.myapplication.DataStorage.PhotoDatabase;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     PhotoDatabase photoDatabase;
     PhotoDao photoDao;
     Button buttonSend, buttonLeft, buttonRight, buttonSnap, buttonSearch;
     ImageView img_photo;
+    String currentPhotoPath;
+    static final int REQUEST_TAKE_PHOTO = 100;
+    Uri photoURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +57,56 @@ public class MainActivity extends AppCompatActivity {
         buttonSnap = findViewById(R.id.buttonSnap);
         img_photo = findViewById(R.id.imageView);
 
+        buttonSnap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+    }
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),"images");
+        try{
+            //make sure the directory was created
+            storageDir.mkdir();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        File image = new File(storageDir, imageFileName+".jpg");
+        Log.d("uri",image.getAbsolutePath());
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Create the File where the photo should go
+        File photoFile = null;
+        try {
+            // give value for image file");
+            photoFile = createImageFile();
+
+        } catch (IOException ex) {
+            //   Log.d("fail", "no photo file");
+
+        }
+        // Continue only if the File was successfully created
+        if (photoFile != null) {
+            photoURI = FileProvider.getUriForFile(this,
+                    "com.example.myapplication.fileprovider",
+                    photoFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+        }
+        //  Log.d("log4", "takepictureintent is null");
     }
 
     public void sendMessage(View view){
@@ -51,5 +115,12 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 100){
+            img_photo.setImageURI(photoURI);
+        }
     }
 }
