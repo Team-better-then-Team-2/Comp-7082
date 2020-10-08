b88e3d7d8ea2955096e4b916c5bcd6fa4d9cc0cd
Ryan Eakin <Eakinryan@hotmail.com>
2020-10-07 23:54:54 -0700

    Location search working pt2

--- app/src/main/java/com/example/myapplication/SearchViewActivity.java
+++ app/src/main/java/com/example/myapplication/SearchViewActivity.java
@@ -1,8 +1,5 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +7,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import com.example.myapplication.DataStorage.Photo;
 import com.example.myapplication.DataStorage.PhotoDao;
@@ -22,9 +22,11 @@ public class SearchViewActivity extends AppCompatActivity {
     PhotoDatabase photoDatabase;
     PhotoDao photoDao;
     private String photo_uri;
+    private String location;
     EditText searchKeyWord;
     EditText editTextdateFrom;
     EditText editTextdateTo;
+    EditText searchlocation;
     int dateFrom;
     int dateTo;
 
@@ -42,7 +44,9 @@ public class SearchViewActivity extends AppCompatActivity {
         editTextdateFrom = findViewById(R.id.editTextFromDate);
         editTextdateTo = findViewById(R.id.editTextToDate);
 
+
         searchKeyWord = findViewById(R.id.keywordsEditText);
+        searchlocation = findViewById(R.id.editTextLocaltionName);
         updateView();
 
     }
@@ -69,13 +73,15 @@ public class SearchViewActivity extends AppCompatActivity {
             toast.show();
             Log.d("when keyword empty2", keyword);
         }
-
     }
     public String searchKey(String key){
         String date1;
         String date2;
+        String location1;
         date1 = editTextdateFrom.getText().toString();
         date2 = editTextdateTo.getText().toString();
+        location1 = searchlocation.getText().toString();
+
         if(!date1.isEmpty() && !date2.isEmpty()){
             dateFrom = Integer.parseInt(date1);
             dateTo = Integer.parseInt(date2);
@@ -91,23 +97,31 @@ public class SearchViewActivity extends AppCompatActivity {
             Photo photo = list.get(i);
             String name = photo.getName().toLowerCase();
             String info = photo.getDescription().toLowerCase();
-            if(name.contains(key)||info.contains(key)){
-                if(dateFrom!=0){
-                    String temp = photo.getTimeStamp();
-                    int temp2 = Integer.parseInt(temp.substring(0,8));
-                    Log.d("time", "From " + dateFrom +", To "+dateTo +" , Now: "+temp2);
+            String locationinfo = photo.getLocation();
 
-                    if(temp2>=dateFrom && temp2<=dateTo){
+            if(name.contains(key)||info.contains(key)){
+                if(locationinfo.contains(location1)) {
+                    if (dateFrom != 0) {
+                        String temp = photo.getTimeStamp();
+                        int temp2 = Integer.parseInt(temp.substring(0, 8));
+                        Log.d("time", "From " + dateFrom + ", To " + dateTo + " , Now: " + temp2);
+
+                        if (temp2 >= dateFrom && temp2 <= dateTo) {
+
+                            return photo.getPhoto();
+                        }
+                    } else {
                         return photo.getPhoto();
                     }
-                }else{
-                    return photo.getPhoto();
                 }
             }
         }
         return null;
     }
 
+    public String[] getLocationdata(String photolocation){
+        return photolocation.split(", ", 2);
+    }
 
     void updateView(){
         List<Photo> list = photoDao.getAllPhotos();
